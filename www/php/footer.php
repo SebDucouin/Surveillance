169f2f488fd80d1dae547adebd2beff030597bd0
SebDucouin <sebastien.ducouin@gmail.com>
2014-09-17 21:38:10 +0200

    architecture changée

---
+++ www/php/footer.php
@@ -0,0 +1,16 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.js"></script>
+<div  class ="container"  >
+	<div class="well text-center">
+   
+   
+		<a href="mentions-legales.php" accesskey="7" title="Mentions l&eacute;gales.">Mentions Légales</a> -
+		<a href="contact.php" accesskey="8" title="Contactez Moi.">Contactez nous</a>
+			
+		<p  style="color:blue;">
+			-- METTRE les infos pieds de pages ici (tel mail adresse etc.. ---
+		</p>
+	</div>
+</div>
---
+++ www/php/menu.php
@@ -0,0 +1,27 @@
+
+
+      <div class="container">
+
+        <div class="navbar navbar-default navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.php">Surveillance Citoyenne</a> <!--TODO-->
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="lieux.php" accesskey="1" title="Prestations et Tarifs">Lieu</a></li> <!--TODO-->
+				<li><a href="qui-sommes-nous.php" accesskey="3" title="Qui-Sommes-nous">Niveau politique</a></li> <!--TODO-->
+				<li><a href="contact.php" accesskey="1" title="Prestations et Tarifs">Thème</a></li>	 <!--TODO-->
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
