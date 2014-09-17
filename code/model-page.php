663dbd04a85e5ca2142c1491ef8cd814aed244f4
Aristide <Aristide.martin.consultant@gmail.com>
2014-09-17 21:02:35 +0200

    code sample
    bout de code ayant servi a la reflexion

---
+++ code/model-page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include('php/head.php'); ?>
+	<body >
+		<div id="conteneur">
+			<?php include('php/header.php'); ?>
+			<?php include('php/menu.php'); ?>
+			<div class="container">
+				
+			</div>
+			<?php include('php/footer.php');?>
+		</div>
+	</body>
+</html>
---
+++ code/php/header.php
@@ -0,0 +1,5 @@
+<div id="header">
+	<!--<div id="header-content">
+	<h1>Alshymi : l'accompagnement personnalisé pour vos projets</h1>
+	</div>-->
+</div>
